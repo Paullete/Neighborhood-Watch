e61d3791dabffe82bba501ee6614f1a599b63ea8
paullete <paulettenereah3@gmail.com>
2022-06-17 14:16:26 +0300

    settings code add

--- hood/settings.py
+++ hood/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neibour',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
